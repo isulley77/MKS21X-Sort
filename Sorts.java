1a903fb6cee493ad55dfc58e809a79e51ea6dc88
isulley77 <43052223+isulley77@users.noreply.github.com>
2018-12-06 07:11:06 -0500

    Add files via upload

--- Sorts.java
+++ Sorts.java
@@ -2,19 +2,16 @@ public class Sorts{
 
   public static void selectionSort(int[] ary){
 
-    int[] sortedAry = new int[ary.length];
-    int smallest = ary[1];
+    int smallest = ary[0];
 
-    for(int j = 0; j < ary.length; j++){
-
-      for(int i = 0; i < ary.length; i++){
-          if(ary[i] < smallest){
-            smallest = ary[i];
-            ary[i] = null;
-          }
+    for(int i = 0; i < ary.length; i++){
+      for(int j = i ; j < ary.length; j++){
+        if(ary[j] < smallest){
+          smallest = ary[j];
+          ary[j] = ary[i];
+        }
       }
-      sortedary[j] = smallest;
+      ary[i] = smallest;
     }
-
   }
 }

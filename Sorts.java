d423afb252c6c472e089f3e6d718a45290c31b7d
isulley77 <43052223+isulley77@users.noreply.github.com>
2018-12-08 12:24:54 -0500

    bubble sort

--- Sorts.java
+++ Sorts.java
@@ -14,4 +14,21 @@ public class Sorts{
       ary[i] = smallest;
     }
   }
+
+  public static void bubbleSort(int[] ary){
+    if(!correct()){
+      int holder = 0;
+      for(int i = 0; i < ary.length; i++){
+
+        for(int j = 1; j < ary.length; j++){
+          if(ary[j] < ary[j-1]){
+
+            holder = ary[j-1];
+            ary[j-1] = ary[j];
+            ary[j] = holder;
+          }
+        }
+      }
+    }
+  }
 }
